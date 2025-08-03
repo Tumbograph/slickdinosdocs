25b3e9c80a32f767fa231b4b1b21ba7fc4a61675
Tumbo <tumbograph@gmail.com>
2025-08-03 20:17:12 +0400

    v1

--- docs/intro.md
+++ docs/intro.md
@@ -2,46 +2,49 @@
 sidebar_position: 1
 ---
 
-# Tutorial Intro
+# Slick Dinos Rulebook
 
-Let's discover **Docusaurus in less than 5 minutes**.
+Slick Dinos IP is focused on spreading positive vibes and slickness by crafting cool brand, memetic culture, and advanced technology.
+
+There is a lot to understand before you fully grasp **Slick Dinos Slickness and its whole ECOSYSTEM**.
+
+But let's try to briefly deliver this information to you through **these documents**.
 
 ## Getting Started
 
-Get started by **creating a new site**.
+Get started by visiting Slick Dinos **[website](https://slickdinos.com)** first to understand basics and feel the vibe.
 
-Or **try Docusaurus immediately** with **[docusaurus.new](https://docusaurus.new)**.
+Slick Dinos main pillar is the community and Dino devs are focused on creating value and distributing it in the community **equally and ethically**.
 
 ### What you'll need
 
-- [Node.js](https://nodejs.org/en/download/) version 18.0 or above:
-  - When installing Node.js, you are recommended to check all checkboxes related to dependencies.
+There are several important points that you **MUST** have to become a proud Slick Dino community member:
+- Friendly & Positive vibes
+- Passion to support the community and grow along with it
+- Follow Slick Dino socials and engage (**[Dinos on X](https://slickdinos.com)**)
+- Not have a purpose of flipping our assets for profit
+- Not try to snipe and profit on our projects (We will still make sure you can't)
+- Have a Phantom wallet or alternatives of it (**[How to](https://phantom.com/learn/guides/how-to-create-a-new-wallet)**
 
-## Generate a new site
+### What you'll get
 
-Generate a new Docusaurus site using the **classic template**.
+By actively engaging with the community and following it closely, you have the opportunity to have fun and enjoy while getting rewarded with valuable digital assets.
 
-The classic template will automatically be added to your project after you run the command:
+Everyhing will be tracked and controlled by Slick Dinos platform, everyone in the community will receive respective appreciation and rewards.
 
-```bash
-npm init docusaurus@latest my-website classic
-```
+You don't need to be a developer or KOL (Key Opinion Leader) to receive appreciation or rewards. Passion, motivation and creativity is what we are looking for.
 
-You can type this command into Command Prompt, Powershell, Terminal, or any other integrated terminal of your code editor.
 
-The command also installs all necessary dependencies you need to run Docusaurus.
+### Documents
 
-## Start your site
+Scroll through the Documents section for full understanding of where Slick Dinos come from and where they are headed.
 
-Run the development server:
+Understand the past to grasp the opportunities of the future.
 
-```bash
-cd my-website
-npm run start
-```
+View all existing products and teasers of what's coming next. 
 
-The `cd` command changes the directory you're working with. In order to work with your newly created Docusaurus site, you'll need to navigate the terminal there.
+### API
 
-The `npm run start` command builds your website locally and serves it through a development server, ready for you to view at http://localhost:3000/.
+Integrate Slick Dino tools into your existing project or launch new projects using these tools.
 
-Open `docs/intro.md` (this page) and edit some lines: the site **reloads automatically** and displays your changes.
+Make use of our technology to stay ahead in the crypto space.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Slick Dinos Docs',
+  tagline: 'Dinos are irresistibly Slick',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -20,15 +20,14 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docs.slickdinos.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'SlickDinos', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -79,26 +78,20 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/og_image.png',
       navbar: {
-        title: 'My Site',
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: 'Slick Dinos Logo',
+          src: 'img/logo.png',
         },
         items: [
           {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Developer Docs',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
+          //{to: '/blog', label: 'Blog', position: 'left'},
         ],
       },
       footer: {
@@ -108,7 +101,7 @@ const config = {
             title: 'Docs',
             items: [
               {
-                label: 'Tutorial',
+                label: 'Developer Docs',
                 to: '/docs/intro',
               },
             ],
@@ -117,16 +110,8 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
                 label: 'X',
-                href: 'https://x.com/docusaurus',
+                href: 'https://x.com/slickdinos',
               },
             ],
           },
@@ -134,17 +119,17 @@ const config = {
             title: 'More',
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: 'IP Rights',
+                href: 'https://slickdinos.com/ip-rights',
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: 'Media Kit',
+                href: 'https://slickdinos.com/media-kit',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Slick Dinos`,
       },
       prism: {
         theme: prismThemes.github,
